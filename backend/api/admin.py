855432ce0bfebed6f9a590b243daf1868f2564b7
flavioborgesnunes <innovatoreegenharia@gmal.com>
2025-01-18 10:38:39 -0300

    Initial commit

---
+++ backend/api/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+
+from api import models as api_models
+
+# class UserAdmin(admin.ModelAdmin):
+#     search_fields = ['full_name', 'username', 'email']
+#     list_display = ['username', 'email']
+
+
+# class ProfileAdmin(admin.ModelAdmin):
+#     search_fields = ['user']
+#     list_display = ['thumbnail', 'user', 'full_name']
+
+
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ["title"]
+
+
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ["title", "user", "category", "view"]
+
+
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ["post", "name", "email", "comment"]
+
+
+# class BookmarkAdmin(admin.ModelAdmin):
+#     list_display = ["user", "post"]
+
+
+# class NotificationAdmin(admin.ModelAdmin):
+#     list_display = ["user", "post", "type", "seen",]
+
+
+admin.site.register(api_models.User)
+admin.site.register(api_models.Profile)
+admin.site.register(api_models.Category)
+admin.site.register(api_models.Post)
+admin.site.register(api_models.Comment)
+admin.site.register(api_models.Notification)
+admin.site.register(api_models.Bookmark)
